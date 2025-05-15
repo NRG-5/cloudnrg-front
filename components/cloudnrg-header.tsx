99bdfe6465aba51c72746a9ee56b3b36e8cd8f5d
Al3xiel <gabriel280104casas@gmail.com>
2025-05-15 18:55:29 -0500

    feat: update header logo

--- components/cloudnrg-header.tsx
+++ components/cloudnrg-header.tsx
@@ -11,7 +11,7 @@ export default function CloudNRGHeader(){
                     orientation="vertical"
                     className="mx-2 data-[orientation=vertical]:h-4"
                 />
-                <h1 className="text-base font-medium">Documents</h1>
+                <h1 className="text-base font-medium">File Manager</h1>
             </div>
         </header>
     );
