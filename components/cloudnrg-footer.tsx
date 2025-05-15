ea2c740c38392cda59506c17acb3852e38fa3f54
neor1se <defneorise@gmail.com>
2025-05-15 18:03:07 -0500

    feat: add footer

---
+++ components/cloudnrg-footer.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator"
+
+const fastLinks = [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "Terms of Service", href: "/terms-of-service" },
+    { name: "Privacy Policy", href: "/privacy-policy" },
+    { name: "Contact Us", href: "/contact" }
+]
+
+export default function CloudNRGFooter(){
+    return (
+        <footer className={`flex flex-row justify-between p-2 border-t `}>
+            <div className={`flex flex-row gap-4`}>
+                { fastLinks.map(({name, href},index) => (
+                 <div key={index} className={`flex flex-row gap-4`}>
+                     <Link href={href} className={`hover:underline`}>
+                         {name}
+                     </Link>
+                     <Separator orientation="vertical" />
+                 </div>
+                )) }
+            </div>
+            <div>
+                CloudNRG SAC © 2025, made with ❤️ by NRG-5 Team
+            </div>
+        </footer>
+    );
+}
